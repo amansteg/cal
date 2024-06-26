4b8f865c31d3a75948a574bc173ecd58b854adc1
amansteg <amanjoshi@stegpearl.com>
2024-06-26 15:32:58 +0530

    2nd class

---
+++ tuts.js
@@ -0,0 +1,41 @@
+
+
+
+
+// Ternary Operator
+let age = 20;
+let result = age>18?"adult":"not adult";
+//console.log(result);
+
+let a = prompt("enter val");
+//console.log(a);
+
+const student = {
+    name: "Aman",
+    age: 24,
+    profession: "developer",
+}
+
+for(let key in student){
+    console.log("key=", key, "value=", student[key]);
+}
+
+
+let str = "JavaScript";
+
+for(let i of str){
+    console.log(i)
+    i++;
+}
+
+
+
+// infite loop
+
+for(let i = 1; i>= 0; i++){
+    console.log(i);
+}
+
+
+
+
