309b3e169a7b3aa1fbd50cb5d922d18c1b8f1e0b
amansteg <amanjoshi@stegpearl.com>
2024-06-27 14:59:06 +0530

    class 3

--- tuts.js
+++ tuts.js
@@ -1,41 +1,81 @@
+//Prompt Answer
+
+// let guessNum = 25;
+
+// let userNum = prompt("Enter the numer");
+// while(userNum != guessNum){
+//      userNum = prompt("You entered wrong number");
+// }
+// alert("Yayyyyy");
 
 
+//Strings
+let a = "JavaScript"; //Coding standard
+let b = 'Javascript'; 
+
+//console.log(a[0]);   //array and string start pos from 0 always
 
 
-// Ternary Operator
-let age = 20;
-let result = age>18?"adult":"not adult";
+const stu = {
+    name: "Aman",
+    class: 12
+}
+
+
+ //console.log("My name is", stu.name, "and i am in class", stu.class);
+
+//Template Literals
+
+let result = `My name is ${stu.name} and i am in class ${stu.class}`;
+
+//${stu.name} is known as String interpolation
 //console.log(result);
 
-let a = prompt("enter val");
-//console.log(a);
 
-const student = {
-    name: "Aman",
-    age: 24,
-    profession: "developer",
-}
-
-for(let key in student){
-    console.log("key=", key, "value=", student[key]);
-}
+let name = "AmanJoshi" //Escape characters
 
 
-let str = "JavaScript";
+//Strings in JS are immutable 
+//Arrays and objects are mutable
 
-for(let i of str){
-    console.log(i)
-    i++;
-}
+//String methods
+
+//.toUpperCase - Convert whole strng in upper case
+//.toLowerCase
+//.trim
+
+//.slice
+//.concat
+//.replace
+//.charAt(5)
 
 
 
-// infite loop
-
-for(let i = 1; i>= 0; i++){
-    console.log(i);
-}
+//ARRAYS
+//Collection of similar datatype - linear [1,5,6,54]
 
 
+// console.log( typeof arr);
+
+// arr[0] = 12;
+// console.log(  arr);
 
 
+arr.splice(1, 100)
+console.log(arr);
+
+
+
+
+//ARRAY Function 
+// push() = add in end
+//pop() = remove from end
+
+
+let arr = ["bloomberg", "microsoft", "Uber", "Google", "IBM", "Netflix"];
+
+
+let price = [240, 400, 300, 768, 900];
+//10% discount 240 - 24 = 216
+
+price = [212, 360, 270, ]
